c5173c6ee6cd2fd07b20ac6a62c9c9cdb5b0eefc
ByJackio <agama@procomi.com>
2026-04-20 15:29:51 -0600

    🎉 Ajustes iniciales del proyecto

--- BlackBarberAPI/Services/AnadidoServicioService.cs
+++ BlackBarberAPI/Services/AnadidoServicioService.cs
@@ -1,34 +1,77 @@
-﻿using BlackBarberAPI.DTOs;
+﻿using AutoMapper;
+using BlackBarberAPI.DTOs;
+using BlackBarberAPI.Models;
 using BlackBarberAPI.Services.Contratos;
 using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BlackBarberAPI.Services
 {
     public class AnadidoServicioService<T> : IAnadidoServicioService<T> where T : DbContext
     {
-        public Task<AnadidoServicioDTO> CrearYObtener(AnadidoServicioDTO objeto)
+        private readonly IGenericRepository<AnadidoServicio, T> _repositorio;
+        private readonly IMapper _Mapper;
+
+        public AnadidoServicioService(IGenericRepository<AnadidoServicio, T> repositorio, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _repositorio = repositorio;
+            _Mapper = mapper;
         }
 
-        public Task<RespuestaDTO> Editar(AnadidoServicioDTO objeto)
+        public async Task<AnadidoServicioDTO> CrearYObtener(AnadidoServicioDTO objeto)
         {
-            throw new NotImplementedException();
+            var modelo = _Mapper.Map<AnadidoServicio>(objeto);
+            var objetoCreado = await _repositorio.Crear(modelo);
+            return _Mapper.Map<AnadidoServicioDTO>(objetoCreado);
         }
 
-        public Task<RespuestaDTO> Eliminar(int id)
+        public async Task<RespuestaDTO> Editar(AnadidoServicioDTO objeto)
         {
-            throw new NotImplementedException();
+            RespuestaDTO respuesta = new RespuestaDTO();
+            var objetoEncontrado = await _repositorio.Obtener(a=>a.Id== objeto.Id);
+            if(objetoEncontrado.Id<=0 || objetoEncontrado == null)
+            {
+                respuesta.Estatus = false;
+                respuesta.Descripcion = "No se encontro el añadido.";
+                return respuesta;
+            }
+            objetoEncontrado.Descripcion= objeto.Descripcion;
+            objetoEncontrado.Nombre = objeto.Nombre;
+            objetoEncontrado.Precio = objeto.Precio;
+            respuesta.Estatus = await _repositorio.Editar(objetoEncontrado);
+            respuesta.Descripcion = respuesta.Estatus ? "Añadido editado exitosamente." : "Ocurrió un error al intentar editar el añadido.";
+            return respuesta;
         }
 
-        public Task<AnadidoServicioDTO> ObtenerXId(int id)
+        public async Task<RespuestaDTO> Eliminar(int id)
         {
-            throw new NotImplementedException();
+            RespuestaDTO respuesta = new RespuestaDTO();
+            var objetoEncontrado = await _repositorio.Obtener(a => a.Id == id);
+            if (objetoEncontrado.Id <= 0 || objetoEncontrado == null)
+            {
+                respuesta.Estatus = false;
+                respuesta.Descripcion = "No se encontro el añadido.";
+                return respuesta;
+            }
+            respuesta.Estatus = await _repositorio.Eliminar(objetoEncontrado);
+            respuesta.Descripcion = respuesta.Estatus ? "Añadido eliminado exitosamente." : "Ocurrió un error al intentar eliminar el añadido.";
+            return respuesta;
         }
 
-        public Task<List<AnadidoServicioDTO>> ObtenerXPerteneciente(int idPerteneciente)
+        public async Task<AnadidoServicioDTO> ObtenerXId(int id)
         {
-            throw new NotImplementedException();
+            var respuesta = await _repositorio.Obtener(a => a.Id == id);
+            if(respuesta == null)
+            {
+                return new AnadidoServicioDTO();
+            }
+            return _Mapper.Map<AnadidoServicioDTO>(respuesta);
+        }
+
+        public async Task<List<AnadidoServicioDTO>> ObtenerXPerteneciente(int idPerteneciente)
+        {
+            var lista = await _repositorio.ObtenerTodos(a=>a.IdPerteneciente == idPerteneciente);
+            return _Mapper.Map<List<AnadidoServicioDTO>>(lista);
         }
     }
 }
---
+++ BlackBarberAPI/Services/Contratos/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BlackBarberAPI.Services.Contratos
+{
+    public interface IGenericRepository<TModel, T>
+    {
+        Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro);
+        Task<List<TModel>> ObtenerTodos(Expression<Func<TModel, bool>> filtro);
+        Task<List<TModel>> ObtenerTodos();
+        Task<TModel> Crear(TModel modelo);
+        Task<bool> CrearMultiple(List<TModel> modelo);
+        Task<bool> EditarMultiple(List<TModel> modelo);
+        Task<bool> Editar(TModel modelo);
+        Task<bool> Eliminar(TModel modelo);
+        Task<bool> EliminarMultiple(List<TModel> modelo);
+    }
+}
